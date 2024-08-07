b2f252540558ba1196f4fe5fc06fc70a45ea8f64
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-08-07 17:37:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,73 @@ function generateSource(index) {
 }
 
 // Add Main Menu
+const contents = document.querySelector(".contents");
+    for (let i = 0; i < categories.length; i++) {
+        let content = document.createElement("div");
+        content.classList.add("content");
+        content.innerHTML = `
+        <div class="image">
+            <img src="${generateSource(i)}" alt="" />
+            <button data-thumb="${i}" data-name="${names[i]}" />
+            <i class="fa-solid fa-cart-plus"></i>
+            <span>Add to Cart</span>
+            </button>
+        </div>
+        <p class="category">${categories[i]}</p>
+        <h2 class="name">${names[i]}</h2>
+        <p class="price">$<span>${prices[i]}</span></p>
+        `;
+        contents.appendChild(content);
+    }
 
+    window.addEventListener("resize", function () {
+        const images = document.querySelectorAll(".image img");
+        for (let j = 0; j < images.length; j++) {
+            images[j].src = generateSource(j);
+        }
+    });
+
+    function updateCartUI() {
+        if (cartList.length > 0) {
+            cart.textContent = "";
+        } else {
+            cart.innerHTML = `
+            <div class="empty-cart">
+                <img class="empty-img" src="./assets/images/illustration-empty-cart.svg" alt="" />
+                <p>Your added items will appear here</p>
+            </div>`;
+            if (cartList.length == 0) {
+                addBtns.forEach((btn, j) => {
+                    btn.innerHTML = `
+                        <i class="fa-solid fa-cart-plus"></i>
+                        <span>Add to Cart</span>`;
+                });
+            }
+            return;
+        }
+
+        cartList.forEach((it, index) => {
+            let item = document.createElement("div");
+            item.classList.add("item");
+            item.innerHTML = `
+                <div class="product-info">
+                    <h3 class="name">${it.name}</h3>
+                    <div class="num-items">
+                        <div class="quantity"><span>${it.quantity}</span></div>
+                        <div class="price-box">
+                            <span class="single-item-price">@$<span>${it.price}</span></span>
+                            <span class="total-price">$<span>$<span>${it.quantity * parseFloat(it.price).toFixed(2)}</span></span>
+                        </div>
+                    </div>
+                </div>
+                <button class="remove-item" data-name="${it.name}" data-index="${index}">
+                <i class="fa-solid fa-times"></i>
+                </button>
+              </div>`;
+
+              
+        })
+    }
 
 
 
